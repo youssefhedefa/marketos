b4ee9611411fd6811379236c79800c57ddddcbc6
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2024-07-25 14:33:27 +0300

    Create sign in states

---
+++ lib/features/registration/logic/cubits/sigin_in_cubit/sign_in_states.dart
@@ -0,0 +1,13 @@
+abstract class SignInStates{}
+
+class SignInInitialState extends SignInStates{}
+
+class SignInLoadingState extends SignInStates{}
+
+class SignInSuccessState extends SignInStates{}
+
+class SignInErrorState extends SignInStates{
+  final String error;
+
+  SignInErrorState(this.error);
+}

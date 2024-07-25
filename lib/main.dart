0bb07a6f0771bf3544ca728227673fe5ff4ecb22
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2024-07-25 14:21:29 +0300

    Add dependency injection file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:marketos/core/di/di.dart';
 import 'package:marketos/core/routing/routing_manager.dart';
 import 'package:marketos/firebase_options.dart';
 import 'package:marketos/marketos.dart';
@@ -9,6 +10,7 @@ void main() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  setupDependencyInjection();
   runApp(Marketos(appRoutingManager: AppRoutingManager(),),);
 }
 

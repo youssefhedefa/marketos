2fb7a3e43dc715f7a818c6ab86f7f0b0ae12e5df
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2024-07-25 14:04:46 +0300

    Add firebase services to the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:marketos/core/routing/routing_manager.dart';
+import 'package:marketos/firebase_options.dart';
 import 'package:marketos/marketos.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(Marketos(appRoutingManager: AppRoutingManager(),),);
 }
 

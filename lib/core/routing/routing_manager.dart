50551ebfc626d64a1418b08b796909ae4471211b
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2024-08-01 18:46:22 +0300

    Provide bloc to log in form

--- lib/core/routing/routing_manager.dart
+++ lib/core/routing/routing_manager.dart
@@ -3,22 +3,31 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marketos/core/di/di.dart';
 import 'package:marketos/core/routing/routing_constants.dart';
 import 'package:marketos/features/home/ui/home_view.dart';
+import 'package:marketos/features/registration/logic/cubits/log_in_cubit/log_in_cubit.dart';
 import 'package:marketos/features/registration/logic/cubits/sign_in_cubit/sign_in_cubit.dart';
 import 'package:marketos/features/registration/ui/forms/log_in_form.dart';
 import 'package:marketos/features/registration/ui/forms/sign_in_form.dart';
 import 'package:marketos/features/splash/ui/splash_view.dart';
 
-class AppRoutingManager{
-  Route onGenerateRoute(RouteSettings settings){
-    switch(settings.name){
+class AppRoutingManager {
+  Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
       case AppRoutingConstants.splash:
         return MaterialPageRoute(builder: (_) => const SplashView());
       case AppRoutingConstants.signIn:
-        return MaterialPageRoute(builder: (_) => BlocProvider.value(
-          value: getIt<SignInCubit>(),
-            child: const SignInForm(),),);
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider.value(
+            value: getIt<SignInCubit>(),
+            child: const SignInForm(),
+          ),
+        );
       case AppRoutingConstants.logIn:
-        return MaterialPageRoute(builder: (_) => const LoginForm());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider.value(
+            value: getIt<LogInCubit>(),
+            child: const LoginForm(),
+          ),
+        );
       case AppRoutingConstants.home:
         return MaterialPageRoute(builder: (_) => const HomeView());
       default:

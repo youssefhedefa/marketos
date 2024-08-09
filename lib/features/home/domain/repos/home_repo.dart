349db22e3b469a4a35dc170ace1d77fb395cd8e2
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2024-08-09 16:04:44 +0300

    Refactor home repo, and home product entity

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:marketos/core/failure/failure.dart';
+import 'package:marketos/features/home/data/models/category_model.dart';
 
 abstract class HomeRepo{
-  Future<Either<dynamic,dynamic>> getHomeCategories();
-  Future<Either<dynamic,dynamic>> getProductsByCategory();
+  Future<Either<Failure,List<CategoryModel>>> getHomeCategories();
+  Future<Either<Failure,dynamic>> getProductsByCategory();
 }

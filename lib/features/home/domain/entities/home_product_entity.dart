bf8992d2880fafe11edb49d56b900f2bfe56c19d
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2024-08-09 15:46:42 +0300

    Create home product entity.

--- lib/features/home/domain/entities/home_product_entity.dart
+++ lib/features/home/domain/entities/home_product_entity.dart
@@ -1,9 +1,13 @@
 class HomeProductEntity{
+  final int id;
   final String name;
   final String price;
+  final String image;
 
   HomeProductEntity({
+    required this.id,
     required this.name,
     required this.price,
+    required this.image,
   });
 }
